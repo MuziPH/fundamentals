86d6e5582ca7945c71ce25141a09fb97f4d4a457
Muzi Phage <muzi.phage@gmail.com>
2024-01-28 12:40:19 +0200

    Accessing Data with Spring Boot and H2

---
+++ src/main/java/com/pluralsight/FundamentalsApplication.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import com.pluralsight.entity.Application;
+import com.pluralsight.repository.ApplicationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+	private static final Logger log = LoggerFactory.getLogger(FundamentalsApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(ApplicationRepository applicationRepository){
+		return (args)->{
+			applicationRepository.save(new Application("Trackzilla", "kesha.williams","Application for tracking bugs"));
+			applicationRepository.save(new Application("Expenses","mary.jones","Application to track expense reports."));
+			applicationRepository.save(new Application("Notifications","karen.kane","Application to send alerts and notifications to users."));
+
+			for (Application application : applicationRepository.findAll()) {
+				log.info("The application is: " + application.toString());
+			}
+
+		};
+	}
+
+}
